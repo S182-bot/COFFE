9d80428953ce7034a3a7e28bb3c9d7886060e3cd
lols <stevenandrew.flores@ici.edu.ph>
2025-02-25 09:07:56 +0300

    Add files via upload

---
+++ First.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$a = 5;
+$b = 5.34;
+$c = "25";
+$d = true;
+
+var_dump($a);
+echo "<br>";
+var_dump($b);
+echo "<br>";
+var_dump($c);
+echo "<br>";
+var_dump($d);
+?> 
+
+
+
+</body>
+</html>
